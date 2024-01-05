6bbc5e0615d796a955f7befa7fb373c7f0a645f3
hafidjs <afidzpratama@gmail.com>
2024-01-05 11:13:22 +0700

    first commit spring apis blog

---
+++ src/test/java/com/hafidtech/blog/blogappapis/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hafidtech.blog.blogappapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
