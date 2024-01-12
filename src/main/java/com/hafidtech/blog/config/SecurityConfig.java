6a18a03428e3bb050cf8cde799393df020bd27cb
hafidjs <afidzpratama@gmail.com>
2024-01-12 21:44:57 +0700

    add spring security

---
+++ src/main/java/com/hafidtech/blog/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.hafidtech.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // ...
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
